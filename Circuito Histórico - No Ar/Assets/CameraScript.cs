9ef27375f46e996baf58972307040b7187ec5905
Cleiton Braga <cleitonbraga56@gmail.com>
2019-09-11 02:55:14 -0300

    Atualizando o projeto - Versão 2019.0

---
+++ Circuito Histórico - No Ar/Assets/CameraScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private Quaternion Rota;
+
+    private Vector3 Cam;
+
+    public Vector3 Camera;
+
+    private Animation Anm;
+
+    public bool Reset;
+
+    void Start()
+    {
+        Rota = Quaternion.Euler(0, 270, 0);
+
+        Anm = GetComponent<Animation>();
+
+        Reset = false;
+    }
+
+    void Update()
+    {
+        Camera = GameObject.Find("Camera").transform.eulerAngles;
+
+        Debug.Log("X: " + Camera.x + "; Y: " + Camera.y + "; Z: " + Camera.z);
+
+        if (Input.GetKeyDown("space") && Anm.isPlaying == false)
+        {
+            Debug.Log("Prefiro morrer do quê perder a vida");
+            Anm.Play();
+        }
+
+        if (Anm.isPlaying)
+        {
+            Debug.Log("Animação em andamento");
+            Debug.Log("Dando umas olhadas");
+
+            Reset = true;
+        }
+
+        if (Reset == true && !Anm.isPlaying)
+        {
+            ResetRotacao();
+        }
+    }
+
+    void ResetRotacao()
+    {
+        Debug.Log("Zerando la rotacion");
+
+        GameObject.Find("Camera").transform.rotation = Rota;
+        GameObject.Find("Camera").transform.eulerAngles = new Vector3(0, 270, 0);
+
+        Reset = false;
+    }
+}
+
+
+
+
+
+
